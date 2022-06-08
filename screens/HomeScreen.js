9fa2fb3ea23e95abae8971990a1800b820322ff7
RadoslavKolev <radoslav.kolev@abv.bg>
2022-06-08 15:11:49 +0300

    Implemented Google Autocomplete Library - Search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar, Image } from 'react-native';
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => { 
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={[
       tw`bg-white h-full`, styles.AndroidSafeArea
@@ -16,6 +22,39 @@ const HomeScreen = () => {
           source={{
             uri: "https://links.papareact.com/gzs",
           }}
+        />
+
+        <GooglePlacesAutocomplete 
+          placeholder="Where From?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            // Push the data into the Redux store
+            dispatch(setOrigin({
+              // Gives an object with latitude and longitude
+              // Stores it in the Redux store
+              location: details.geometry.location,
+              description: data.description
+            }));
+
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}  // Removes Powered by Google
+          minLength={2}
+          query={{  // links to the private API key and does the autocomplete
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
         /> 
 
         <NavOptions /> 
